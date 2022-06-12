2030810ed95ecd0328182f2736db819785c5b92e
jwjung5038 <jwjung5038@gmail.com>
2022-06-12 17:47:42 +0900

    fix: cors

--- src/main/kotlin/day/toons/global/config/WebConfig.kt
+++ src/main/kotlin/day/toons/global/config/WebConfig.kt
@@ -1,4 +1,12 @@
 package day.toons.global.config
 
-class WebConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer{
+    override fun addCorsMappings(registry: CorsRegistry) {
+        super.addCorsMappings(registry)
+    }
 }
