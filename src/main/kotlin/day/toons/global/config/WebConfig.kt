542163e481b98be52a914e09495d98f566a5f7c2
jwjung5038 <jwjung5038@gmail.com>
2022-07-22 22:14:24 +0900

    fix: @MemberAuth argument handler 등록

--- src/main/kotlin/day/toons/global/config/WebConfig.kt
+++ src/main/kotlin/day/toons/global/config/WebConfig.kt
@@ -1,11 +1,18 @@
 package day.toons.global.config
 
+import day.toons.global.config.security.MemberPrincipalMethodArgumentResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig: WebMvcConfigurer{
+class WebConfig : WebMvcConfigurer {
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(MemberPrincipalMethodArgumentResolver())
+    }
+
     override fun addCorsMappings(registry: CorsRegistry) {
         super.addCorsMappings(registry)
     }
